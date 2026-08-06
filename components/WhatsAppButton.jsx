89bbca29bdf7baf4da29e085888fba6cdb022459
Reyansh <reyanshsingh2257@gmail.com>
2026-08-06 22:21:16 +0530

    updated website

--- components/WhatsAppButton.jsx
+++ components/WhatsAppButton.jsx
@@ -3,7 +3,7 @@
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/YOURNUMBER"
+      href="https://wa.me/7399899994"
       target="_blank"
       rel="noopener noreferrer"
       className="whatsapp"
